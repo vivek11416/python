295617841054224feb9892893d394e3081ee6a15
vivek.pal <vivekkumar.en@gmail.com>
2021-07-19 11:46:51 +0530

    updated code

--- factorial.py
+++ factorial.py
@@ -6,4 +6,14 @@ def factorial(n):
         return n*factorial(n-1)
 
 
-print(factorial(-4))
+#print(factorial(-4))
+
+def fibonacci(n):
+    assert n>=0 and int(n) == n , 'The number must be positive integer only !'
+    if n in [0,1]:
+        return n
+    else:
+        
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(-7))
